b0738c2c91be62aae6c8850d7aa31d37ddcee2da
俞曾 <981952053@qq.com>
2023-03-28 00:30:07 +0800

    213

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,26 +3,34 @@
 import gsap from 'gsap';
 
 import calendar from "yz-calendar";
-import { onMounted } from "vue";
+import { onMounted, onUpdated } from "vue";
 // console.log(calendar.solar2lunar(2021, 3, 21));
 
 import { useRouter } from "vue-router";
 
 
-import Button from "@/components/home/table";
+import Table from "@/components/home/table";
 
 const router = useRouter();
 
 
 
-
-// onMounted(() => {
-//     const line = gsap.timeline();
-//     line.from(".table ", { duration: 4, ease: "power2", opacity: 0, y: -1600 })
-// })
+onMounted(() => {
+    setTimeout(() => {
+        const line = gsap.timeline();
+        line.from(".thistable ", { duration: 2, opacity: 0, ease: "power2", display: "block", y: -1700 })
+        line.to(".thistable ", { duration: 2, opacity: 1, display: "block", y: 0 })
+    }, 300)
+})
 
 </script>
 
 <template>
-    <Button class="table"></Button>
+    <Table class="thistable"></Table>
 </template>
+<style scoped>
+.thistable {
+    display: none;
+    transform: translateY(-1700);
+}
+</style>
